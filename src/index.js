1d95f7b79e9bc758487f9f08d0538cb5e768db3e
toda93 <dttong93@gmail.com>
2020-02-09 17:50:45 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import signMiddleware from './middleware/signMiddleware';
+import etagMiddleware from './middleware/etagMiddleware';
 
 import { startServer } from './server';
 
 export {
 	startServer,
     signMiddleware,
+    etagMiddleware,
 }
---
+++ src/middleware/etagMiddleware.js
@@ -0,0 +1,13 @@
+import { isValidSign } from '@azteam/crypto';
+import { ErrorException, SIGNATURE_FAILED } from '@azteam/error';
+import etag from 'etag';
+
+export default (req, res, next) => {
+    if (req.method === 'GET') {
+        const etag_hash = etag(req.url);
+        if (req.headers['if-none-match'] === etag_hash) {
+            return res.status(304).send();
+        }
+        res.setHeader('ETag', etag_hash);
+    }
+};
