4fce188c95eadddb79aeddfb3de8f8eae59c0998
toda93 <dttong93@gmail.com>
2020-02-19 17:59:19 +0700

    sync

--- src/middleware/signMiddleware.js
+++ src/middleware/signMiddleware.js
@@ -1,22 +1,26 @@
-import {isValidSign} from '@azteam/crypto';
-import {ErrorException, SIGNATURE_FAILED} from '@azteam/error';
+import { isValidSign } from '@azteam/crypto';
+import { ErrorException, SIGNATURE_FAILED } from '@azteam/error';
 import etag from 'etag';
 
-export default (req, res, next) => {
-    if (req.query.sign) {
-        if (req.method === 'GET') {
-            const etag_hash = etag(req.url);
-            if (req.headers['if-none-match'] === etag_hash) {
-                return res.status(304).send();
-            }
-            res.setHeader('ETag', etag_hash);
-        }
-        const secretKey = req.app.get('SECRET_KEY');
-        let url = `${req.protocol}://${req.hostname}${req.originalUrl}`;
-        if (isValidSign(url, secretKey)) {
-            return next();
-        }
-    }
-    throw new ErrorException(SIGNATURE_FAILED);
-};
 
+
+
+export default (mTimeout = 5) => {
+    return async (req, res, next) => {
+        if (req.query.sign) {
+            if (req.method === 'GET') {
+                const etag_hash = etag(req.url);
+                if (req.headers['if-none-match'] === etag_hash) {
+                    return res.status(304).send();
+                }
+                res.setHeader('ETag', etag_hash);
+            }
+            const secretKey = req.app.get('SECRET_KEY');
+            let url = `${req.protocol}://${req.hostname}${req.originalUrl}`;
+            if (isValidSign(url, secretKey, mTimeout)) {
+                return next();
+            }
+        }
+        throw new ErrorException(SIGNATURE_FAILED);
+    }
+}
