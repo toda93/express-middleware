cb3d33c906b74d50ba6b1b7df69f954192c42514
toda93 <dttong93@gmail.com>
2020-02-25 00:16:04 +0700

    sync

--- src/middleware/etagMiddleware.js
+++ src/middleware/etagMiddleware.js
@@ -2,9 +2,17 @@ import { isValidSign } from '@azteam/crypto';
 import { ErrorException, SIGNATURE_FAILED } from '@azteam/error';
 import etag from 'etag';
 
+
+
+function floorToMinute(time, minutes) {
+    const roundSecond = minutes * 60;
+    time = time - (time % (Math.floor(time / roundSecond) * roundSecond));
+    return time;
+}
+
 export default (req, res, next) => {
     if (req.method === 'GET') {
-        const etag_hash = etag(req.url);
+        const etag_hash = etag(req.url) + floorToMinute(Math.floor(Date.now() / 1000), 5);
         if (req.headers['if-none-match'] === etag_hash) {
             return res.status(304).send();
         }
