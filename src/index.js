e69f7afea04614c8d86cf5b60c042ccaf4c64e2a
toda93 <dttong93@gmail.com>
2020-03-11 14:06:02 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ServiceRegister from './ServiceRegister';
 
 import signMiddleware from './middleware/signMiddleware';
 import etagMiddleware from './middleware/etagMiddleware';
+import paginateMiddleware from './middleware/paginateMiddleware';
 
 
 export {
@@ -11,4 +12,5 @@ export {
 
     signMiddleware,
     etagMiddleware,
+    paginateMiddleware,
 }
