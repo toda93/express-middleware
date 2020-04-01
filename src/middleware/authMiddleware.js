8b55d9207cab65d1042ba40a1107bf881ed832c2
toda93 <dttong93@gmail.com>
2020-04-01 23:39:33 +0700

    sync

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 import HttpClient from '@azteam/http-client';
-import {ErrorException, TOKEN_EXPIRED} from '@azteam/error';
+import { ErrorException, TOKEN_EXPIRED } from '@azteam/error';
 import jwt from 'jsonwebtoken';
 
 
@@ -31,33 +31,33 @@ async function getInfoByAPIToken(endpoint, token) {
 export default (cb_refresh_token, cb_login_api) => {
     return async (req, res, next) => {
         let token = req.signedCookies.access_token || req.headers.authorization;
-
-        if (token && token.startsWith('Bearer ')) {
+        if (token) {
             token = token.replace('Bearer ', '');
-        }
 
-        return jwt.verify(token, process.env.SECRET_KEY, async (error, jwt_data) => {
-            if (error) {
-                let data;
-                if (error.name === 'TokenExpiredError') {
-                    if (req.signedCookies.refresh_token) {
-                        data = await cb_refresh_token(req.signedCookies.refresh_token);
-                    } else {
-                        throw new ErrorException(TOKEN_EXPIRED)
+            return jwt.verify(token, process.env.SECRET_KEY, async (error, jwt_data) => {
+                if (error) {
+                    let data;
+                    if (error.name === 'TokenExpiredError') {
+                        if (req.signedCookies.refresh_token) {
+                            data = await cb_refresh_token(req.signedCookies.refresh_token);
+                        } else {
+                            throw new ErrorException(TOKEN_EXPIRED)
+                        }
+                    } else if (error.name === 'JsonWebTokenError') {
+                        data = await cb_login_api(token);
                     }
-                } else if (error.name === 'JsonWebTokenError') {
-                    data = await cb_login_api(token);
+                    if (data) {
+                        res.cookie('access_token', data.access_token, COOKIE_OPTIONS);
+                        jwt_data = jwt.decode(data.access_token);
+                    }
                 }
-                if (data) {
-                    res.cookie('access_token', data.access_token, COOKIE_OPTIONS);
-                    jwt_data = jwt.decode(data.access_token);
+                if (jwt_data) {
+                    req.user = jwt_data;
                 }
-            }
-            if (jwt_data) {
-                req.user = jwt_data;
-            }
-            return next();
-        });
+                return next();
+            });
+        }
+        return next();    
     };
 
 }
