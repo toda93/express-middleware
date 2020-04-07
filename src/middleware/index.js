2f64b324900aa43605b09663cee09ba197104fba
toda93 <dttong93@gmail.com>
2020-04-08 04:27:52 +0700

    sync

---
+++ src/middleware/index.js
@@ -0,0 +1,13 @@
+import signMiddleware from './signMiddleware';
+import etagMiddleware from './etagMiddleware';
+import authMiddleware from './authMiddleware';
+import roleMiddleware from './roleMiddleware';
+import paginateMiddleware from './paginateMiddleware';
+
+export {
+    signMiddleware,
+    etagMiddleware,
+    paginateMiddleware,
+    authMiddleware,
+    roleMiddleware,
+}
