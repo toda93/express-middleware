9d89360597588c06d37a00e81224ef5bfe194b5f
toda93 <dttong93@gmail.com>
2020-04-09 08:12:20 +0700

    sync

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,4 +1,3 @@
-import HttpClient from '@azteam/http-client';
 import {ErrorException, TOKEN_EXPIRED} from '@azteam/error';
 import jwt from 'jsonwebtoken';
 
@@ -13,38 +12,49 @@ const COOKIES_OPTIONS = {
 
 export default (cb_refresh_token, cb_login_api) => {
     return async (req, res, next) => {
-        let token = req.signedCookies.access_token || req.headers.authorization;
-        if (token) {
-            token = token.replace('Bearer ', '');
+        if (req.headers['APP_KEY'] === process.env.SECRET_KEY) {
+            let user = {};
+            if (req.headers['USER']) {
+                user = JSON.parse(req.headers['USER']);
+            }
+            req.user = user;
+        } else {
+            let token = req.signedCookies.access_token || req.headers.authorization;
+            if (token) {
+                token = token.replace('Bearer ', '');
 
-            return jwt.verify(token, process.env.SECRET_KEY, async (error, jwt_data) => {
-                if (error) {
-                    let data;
-                    if (error.name === 'TokenExpiredError') {
-                        if (req.signedCookies.refresh_token) {
-                            data = await cb_refresh_token(req.signedCookies.refresh_token);
-                        } else {
-                            if (req.signedCookies.api) {
-                                data = await cb_login_api(token);
+                return jwt.verify(token, process.env.SECRET_KEY, async (error, jwt_data) => {
+                    if (error) {
+                        let data;
+                        if (error.name === 'TokenExpiredError') {
+                            if (req.signedCookies.refresh_token) {
+                                data = await cb_refresh_token(req.signedCookies.refresh_token);
+                            } else {
+                                if (req.signedCookies.api) {
+                                    data = await cb_login_api(token);
+                                }
+                                throw new ErrorException(TOKEN_EXPIRED)
                             }
-                            throw new ErrorException(TOKEN_EXPIRED)
+                        } else if (error.name === 'JsonWebTokenError') {
+                            data = await cb_login_api(token);
                         }
-                    } else if (error.name === 'JsonWebTokenError') {
-                        data = await cb_login_api(token);
+
+                        if (data) {
+                            res.addCookie(data);
+                            jwt_data = jwt.decode(data.access_token);
+                        }
                     }
-                    if (data) {
-                        res.addCookie(data);
-                        jwt_data = jwt.decode(data.access_token);
+                    if (jwt_data) {
+                        req.user = jwt_data;
+                    } else {
+                        res.cleanCookie(['access_token', 'refresh_token']);
                     }
-                }
-                if (jwt_data) {
-                    req.user = jwt_data;
-                } else {
-                    res.cleanCookie(['access_token', 'refresh_token']);
-                }
-                return next();
-            });
+                    return next();
+                });
+            }
         }
+
+
         return next();
     };
 
