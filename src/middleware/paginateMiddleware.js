af34d31761480b3f676951c3e30098ebb9e3a43f
Tong Dang <dttong@chopet.vn>
2020-09-12 14:23:40 +0700

    sync

--- src/middleware/paginateMiddleware.js
+++ src/middleware/paginateMiddleware.js
@@ -9,19 +9,21 @@ export default (options = {}) => {
 
     return async (req, res, next) => {
         req.resOptions = options;
-        req.paginate = {};
+        req.paginate = {
+            limit: options.limit
+        };
+        if (req.query.limit) {
+            req.paginate.limit = Number(req.query.limit);
+            delete req.query.limit;
+
+        }
         if (req.query.page) {
             req.paginate.page = Number(req.query.page);
             req.paginate.offset = (req.paginate.page - 1) * req.paginate.limit;
 
             delete req.query.page;
         }
-        if (req.query.limit) {
-            req.paginate.limit = Number(req.query.limit);
-            delete req.query.limit;
-
-        }
-
+        
         if (req.query.sort_by && options.sortFields.includes(req.query.sort_by)) {
             req.paginate.sort = {
                 [req.query.sort_by]: req.query.sort_type === 'asc' ? 'asc' : 'desc' };
