7d45767d04011a0a59e48dfcbec47fae4f469455
Tong Dang <dttong@chopet.vn>
2020-09-13 14:41:24 +0700

    sync

--- src/middleware/roleMiddleware.js
+++ src/middleware/roleMiddleware.js
@@ -1,10 +1,13 @@
 import { ErrorException, UNAUTHORIZED, PERMISSION } from '@azteam/error';
 
 
-export default (roles = null) => {
+export default (roles = null, minLevel = 1) => {
     return async (req, res, next) => {
         if (!req.user) {
             throw new ErrorException(UNAUTHORIZED);
+        }
+        if (req.user.level < minLevel) {
+            throw new ErrorException(PERMISSION);
         }
         if (!roles || req.user.level === 100 || req.user.roles.some(r => roles.includes(r))) {
             return next();
