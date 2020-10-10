057ce8ee69fc0720b342e747cae82a7921469042
Tong Dang <dttong@chopet.vn>
2020-10-10 18:39:08 +0700

    sync

--- src/middleware/omitEmptyMiddleware.js
+++ src/middleware/omitEmptyMiddleware.js
@@ -1,4 +1,30 @@
-import omitEmpty from 'omit-empty';
+function omitEmpty(data) {
+    Object.keys(data).map(function(key, index) {
+        let value = data[key];
+        if (typeof value === 'string') {
+        	
+            value = value.trim();
+
+            if (value === '' || value === 'NaN' || value === 'null' || value === 'undefined') {
+                delete data[key];
+            } else {
+
+                data[key] = value;
+
+                if (!isNaN(value)) {
+                    data[key] = Number(value);
+                }
+            }
+
+        } else {
+
+            if (value === null || value === undefined || Number.isNaN(value)) {
+                delete data[key];
+            }
+        }
+    });
+}
+
 
 
 export default () => {
