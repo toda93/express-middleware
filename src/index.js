0c249c69737a0459d20fa93fa876042dd3dbd01c
Tong Dang <dttong@chopet.vn>
2021-03-22 19:12:45 +0700

    sync

--- src/index.js
+++ src/index.js
@@ -1,30 +1,10 @@
-import {HTTP_METHOD, REQUEST_TYPE} from './constant';
-
 import ApiServer from './ApiServer';
-import signMiddleware from './middleware/signMiddleware';
-import etagMiddleware from './middleware/etagMiddleware';
-import authMiddleware from './middleware/authMiddleware';
-import roleMiddleware from './middleware/roleMiddleware';
-import paginateMiddleware from './middleware/paginateMiddleware';
-import validateMiddleware from './middleware/validateMiddleware';
-import limitRequestMiddleware from './middleware/limitRequestMiddleware';
-
-
 import SocketServer from './SocketServer';
 
+export * from './middleware';
+export * from './constant';
 
 export {
-	HTTP_METHOD, 
-    REQUEST_TYPE,
-
     ApiServer,
     SocketServer,
-
-    signMiddleware,
-    etagMiddleware,
-    paginateMiddleware,
-    authMiddleware,
-    roleMiddleware,
-    validateMiddleware,
-    limitRequestMiddleware
 }
---
+++ src/middleware/index.js
@@ -0,0 +1,8 @@
+export * from './signMiddleware';
+export * from './etagMiddleware';
+export * from './authMiddleware';
+export * from './roleMiddleware';
+export * from './paginateMiddleware';
+export * from './validateMiddleware';
+export * from './limitRequestMiddleware';
+
