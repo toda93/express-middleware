7d584e63b0701d137f4d832351b3928401e64d0d
Tong Dang <dttong@chopet.vn>
2021-03-22 19:26:30 +0700

    sync

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,8 +1,8 @@
-export * from './signMiddleware';
-export * from './etagMiddleware';
-export * from './authMiddleware';
-export * from './roleMiddleware';
-export * from './paginateMiddleware';
-export * from './validateMiddleware';
-export * from './limitRequestMiddleware';
+export {default as signMiddleware} from './signMiddleware';
+export {default as etagMiddleware} from './etagMiddleware';
+export {default as authMiddleware} from './authMiddleware';
+export {default as roleMiddleware} from './roleMiddleware';
+export {default as paginateMiddleware} from './paginateMiddleware';
+export {default as validateMiddleware} from './validateMiddleware';
+export {default as limitRequestMiddleware} from './limitRequestMiddleware';
 
