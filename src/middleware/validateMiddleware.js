edf56e61fae04c987ed382002dc3a3db22a71ec1
Tong Dang <dttong93@gmail.com>
2021-04-26 15:22:13 +0700

    sync

--- src/middleware/validateMiddleware.js
+++ src/middleware/validateMiddleware.js
@@ -4,8 +4,7 @@ import Validator from 'fastest-validator';
 
 const v = new Validator({
     messages: {
-        // Register our new error message text
-        json: "The '{field}' field must be an json string! Actual: {actual}"
+        jsonString: "The '{field}' field must be an json string! Actual: {actual}"
     }
 });
 v.add("json", function({ schema, messages }, path, context) {
@@ -19,7 +18,6 @@ v.add("json", function({ schema, messages }, path, context) {
 
     return {
         source: `
-
             try {
                 JSON.parse(string);
             } catch (e) {
